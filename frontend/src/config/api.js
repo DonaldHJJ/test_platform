d4485bdbf428736af69836d30e3ed3c55f14c197
DonaldHJJ <DonaldHJJ@users.noreply.github.com>
2026-04-07 23:57:11 +0800

    添加启动端口配置

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,6 +1,7 @@
 export default {
   apiBaseUrl: 'http://127.0.0.1:200',
   publicPath: '/',
+  devServerPort: 5173,
   endpoints: {
     runFlow: '/api/runFlow',
     getReport: '/api/getReport'
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import createGroupPlugin from './vite-plugin-create-group.js'
 import path from 'path'
+import config from './src/config/api.js'
 
 export default defineConfig({
   plugins: [vue(), createGroupPlugin()],
   publicDir: 'public',
   server: {
+    port: config.devServerPort,
     fs: {
       allow: ['..']
     }
